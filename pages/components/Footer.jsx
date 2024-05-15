01c92a853535e57154277ef1067951c1406b1a36
dahlica12 <dahliajames2003@gmail.com>
2024-05-14 23:32:23 -0400

    added footer rights reserved message

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Footer = () => {
+  
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
         {/* <span>LOGO</span> */}
-        <p className="text-slate-600"></p>
+        <p className="text-slate-600"> @dahliajamesportfolio.com {currentYear} All rights reserved.</p>
       </div>
     </footer>
   );
